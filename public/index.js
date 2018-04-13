37dcafc6757ba3422395750c476fabe6cb329535
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-13 16:22:45 +0100

    Refactor index.js

--- public/index.js
+++ public/index.js
@@ -1,8 +1,17 @@
 const app = function(){
 
+  // initial map setup - refactor this into a function then call it within app
   const initialCenter = { lat: 55.856806, lng: -4.244106 }
-  var container = document.getElementById('main-map');
+  const container = document.getElementById('main-map');
   const mainMap = new MapWrapper(container, initialCenter, 19);
+  mainMap.addMarker(initialCenter);
+
+  // add marker to map at the coordinates entered for home - refactor this into a function
+  const homeButton = document.getElementById('set-home-button');
+  homeButton.addEventListener('click', setHomeMarker);
+  const setHomeMarker = function(){
+  }
+
 
 }
 
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -1,8 +1,14 @@
 const MapWrapper = function(container, coords, zoom){
-
   this.googleMap = new google.maps.Map(container, {
-    center: coords,//{lat: 55.862241, lng: -4.019337},
-    zoom: zoom
+    center: coords,
+    zoom: zoom,
   });
+
+  MapWrapper.prototype.addMarker = function (coords) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap
+    });
+  };
 
 }
